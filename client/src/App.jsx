2ac618378c4bb1ff1390f0f5f3f743e3c51182c3
Prajwal <prajwalsjp2000@gmail.com>
2025-07-07 12:50:29 +0530

    feat: (frontend init setup) update project structure with routing, theming, and new pages; remove unused CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NotFound from "./pages/NotFound";
+import toast, {Toaster} from 'react-hot-toast'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getInitialTheme = () => {
+    const stored = localStorage.getItem("theme");
+    if (stored) return stored;
+    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return systemPrefersDark ? "dark" : "light";
+  };
+
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Toaster />
+    <button onClick={() => toast.success("Hey look me")} >click here</button>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="*" element={ <NotFound /> } />
+    </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
