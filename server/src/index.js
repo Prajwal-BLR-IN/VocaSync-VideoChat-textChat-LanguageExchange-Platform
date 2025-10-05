84c6aa63a54121f2ecfde2d6f3c40795a679f56b
Prajwal <prajwalsjp2000@gmail.com>
2025-10-05 18:56:51 +0530

    style: Refactor code formatting and improve consistency in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,40 @@
-import express from 'express';
-import 'dotenv/config'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import ConnectToDB from './configs/mongoDB.js';
+import ConnectToDB from "./configs/mongoDB.js";
 
-import authRouter from './routes/auth.route.js';
-import userRouter from './routes/user.route.js';
-import chatRouter from './routes/chat.routes.js';
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import chatRouter from "./routes/chat.routes.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 //middlewares
-app.use(cors({
-    origin: ["http://localhost:5173", "http://192.168.1.8:5173"],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.1.8:5173",
+      "https://vocasync.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API Running ..(🚀)..");
+});
 
-app.get('/', (req, res) => {
-    res.send("API Running ..(🚀)..");
-})
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/chat", chatRouter);
 
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
-app.use('/api/chat', chatRouter);
-
-
-app.listen(PORT, async() => {
-    console.log(`Server running is http://localhost:${PORT}`);
-    await ConnectToDB();
+app.listen(PORT, async () => {
+  console.log(`Server running is http://localhost:${PORT}`);
+  await ConnectToDB();
 });
