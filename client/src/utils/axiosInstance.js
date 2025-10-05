752499f212890d03be0588f0ef48af2a3fb8b4da
Prajwal <prajwalsjp2000@gmail.com>
2025-10-05 18:51:33 +0530

    fix: Correct baseURL assignment in axios instance to use VITE_BASE_URL

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstanace = axios.create({
-  baseURL: process.meta.env.Backend_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   // baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
   withCredentials: true,
 });
