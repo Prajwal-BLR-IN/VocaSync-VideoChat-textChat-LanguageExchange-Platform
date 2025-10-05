c064a8206ddcc1bfdc035535856346c7b31e8bdb
Prajwal <prajwalsjp2000@gmail.com>
2025-10-05 20:03:52 +0530

    fix: Correct baseURL assignment in axios instance to include '/api' path

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstanace = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  // baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
   withCredentials: true,
 });
