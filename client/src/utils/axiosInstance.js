26ae97edef34aa2fc49a63374eeb6a86fc73387f
Prajwal <prajwalsjp2000@gmail.com>
2025-10-05 18:45:25 +0530

    feat: Update axios instance to use dynamic backend URL from environment variables

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstanace = axios.create({
-    baseURL: "http://localhost:4001/api",
-    // baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
-    withCredentials: true
-})
+  baseURL: process.meta.env.Backend_URL,
+  // baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
+  withCredentials: true,
+});
